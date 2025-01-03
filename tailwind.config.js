fffc239c0df5f8ead6a5be1828425d4e63ca518e
BoviliusMeidi <boviliusbm@gmail.com>
2025-01-04 00:54:13 +0700

    feat(dependencies) : add @material-tailwind/react

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+export default withMT({
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -9,10 +11,14 @@ export default {
       colors: {
         primary: '#2B1216',
         secondary: '#15231B',
+        tertiary: '#134857',
         footer: '#1F2040',
         rightBar: '#F4A83A',
       },
+      spacing: {
+        '128': '32rem',
+      }
     },
   },
   plugins: [],
-}
+})
