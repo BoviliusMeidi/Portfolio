a65d67a5e1b6128566ec83ea2993996b46705ad2
BoviliusMeidi <boviliusbm@gmail.com>
2025-01-12 14:33:08 +0700

    style(pages) : changes link cv in hero page

--- src/components/layout/Hero.jsx
+++ src/components/layout/Hero.jsx
@@ -39,7 +39,7 @@ const Hero = ({ id }) => {
             <a href="/about">
               <Title title={"BOVILIUS MEIDI"} />
             </a>
-            <Link link={"/"} text={"Download CV"}>
+            <Link link={"/CV.pdf"} text={"Download CV"}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 height="30"
