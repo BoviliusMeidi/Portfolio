28c8d2f8aab54d7448574281b6e1958b0ee2c2e7
BoviliusMeidi <boviliusbm@gmail.com>
2025-01-14 20:26:27 +0700

    feat(tailwind-css) : add configuration tailwind and css variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,34 @@ export default withMT({
       },
       spacing: {
         '128': '32rem',
-      }
+      },
+      fontSize: {
+        'number': '12rem',
+      },
+      animation: {
+        popin: 'popin 3s ease-out',
+        popout: 'popout 3s ease-out',
+        slidein: 'slidein 3s ease-out',
+        slideout: 'slideout 3s ease-out',
+      },
+      keyframes: {
+        popin: {
+          '0%': { transform: 'translateY(100%)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+        popout: {
+          '0%': { transform: 'translateY(-100%)', opacity: '0' },
+          '100%': { transform: 'translateY(0%)', opacity: '1' },
+        },
+        slidein:{
+          '0%': {transform: 'translateX(0)', opacity: '0'},
+          '100%': {transform: 'translateX(100%)', opacity: '1'}
+        },
+        slideout:{
+          '0%': {transform: 'translateX(100%)', opacity: '0'},
+          '100%': {transform: 'translateX(0%)', opacity: '1'}
+        },
+      },
     },
   },
   plugins: [],
