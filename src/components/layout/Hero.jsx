099b98101c5392cd3a5915dd9c8bc445b8c73f2f
BoviliusMeidi <boviliusbm@gmail.com>
2025-11-04 20:29:06 +0700

    style(components) : change style of the hero section

--- src/components/layout/Hero.jsx
+++ src/components/layout/Hero.jsx
@@ -1,56 +1,74 @@
 import PropTypes from "prop-types";
-import Link from "../common/Link";
 import MainLayout from "./MainLayout";
-import Title from "../common/Title";
 import HeroImage from "../../assets/images/Hero-BoviliusMeidi.png";
 import { TypeAnimation } from "react-type-animation";
 import Quote from "../common/Quote";
+import DownloadCVIcon from"../../assets/icon/download-cv.svg";
 
 const Hero = ({ id }) => {
   const backgroundColor = "bg-primary";
   return (
     <MainLayout backgroundColor={backgroundColor} center={true}>
       <div
-        className="relative flex flex-col py-28 md:py-0 md:justify-center items-center "
+        className="flex flex-col py-28 w-full h-full md:py-0 md:justify-center items-center third-font gradient-and-dragon-bg"
         id={id}
       >
-        <div
-          className={`flex lg:flex-row justify-center items-center lg:gap-32 flex-col gap-3 w-full transition-opacity duration-500 ease-in-out animate-popin`}
-        >
-          <img
-            src={HeroImage}
-            alt="Bovilius Meidi"
-            className="w-60 md:w-96 rounded-full "
-          />
-          <div className="md:text-left w-full">
-            <TypeAnimation
-              sequence={[
-                "FrontEnd Developer",
-                1000,
-                "BackEnd Developer",
-                1000,
-                "Software Engineer",
-                1000,
-              ]}
-              wrapper="span"
-              speed={50}
-              className="lg:text-4xl text-2xl md:text-3xl second-font-bold-italic letter-spacing-10"
-              repeat={Infinity}
-            />
-            <a href="/about">
-              <Title title={"BOVILIUS MEIDI"} />
-            </a>
-            <Link link={"/CV.pdf"} text={"Download CV"}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="30"
-                viewBox="0 -960 960 960"
-                width="30"
-                fill="#000000"
+        <div className="flex lg:flex-row justify-center items-center md:items-start lg:gap-12 flex-col gap-2 w-full transition-opacity duration-500 ease-in-out animate-popin">
+          <a href="/about">
+            <div className="group md:rounded-full overflow-hidden transition-all duration-300 ease-in-out cursor-pointer">
+              <img
+                src={HeroImage}
+                alt="Bovilius Meidi"
+                className="w-full h-full object-cover transition-transform duration-300 ease-in-out group-hover:scale-110"
+              />
+            </div>
+          </a>
+          <div className="flex flex-col justify-center w-full sm:w-fit md:h-full gap-2 sm:gap-16 md:text-left">
+            <div className="flex flex-col justify-center items-center">
+              <a
+                href="/about"
+                className="group relative inline-block overflow-hidden"
               >
-                <path d="M480-313 287-506l43-43 120 120v-371h60v371l120-120 43 43-193 193ZM220-160q-24 0-42-18t-18-42v-143h60v143h520v-143h60v143q0 24-18 42t-42 18H220Z" />
-              </svg>
-            </Link>
+                <h1 className="text-4xl font-bold md:text-7xl lg:text-[7rem] transition-transform duration-500 ease-in-out group-hover:-translate-y-full">
+                  BOVILIUS MEIDI
+                </h1>
+                <h1
+                  className="text-4xl font-bold md:text-7xl lg:text-[7rem] transition-transform duration-500 ease-in-out absolute top-full left-0 group-hover:-translate-y-full"
+                  aria-hidden="true"
+                >
+                  BOVILIUS MEIDI
+                </h1>
+              </a>
+              <TypeAnimation
+                sequence={[
+                  "FrontEnd Developer",
+                  1000,
+                  "BackEnd Developer",
+                  1000,
+                  "Website Developer",
+                  1000,
+                  "Software Engineer",
+                  1000,
+                ]}
+                wrapper="span"
+                speed={50}
+                className="text-xl md:text-4xl lg:text-6xl italic letter-spacing-10"
+                repeat={Infinity}
+              />
+            </div>
+            <div className="flex justify-end mr-6 sm:mr-0">
+              <a
+                href="/CV.pdf"
+                className="flex justify-center text-base sm:text-2xl items-center gap-2 sm:gap-4 px-6 sm:px-12 py-3 sm:py-6 rounded-3xl cursor-pointer no-underline text-white second-font-black letter-spacing-5 transition-colors duration-400 bg-gray-900 hover:bg-rightBar hover:text-black w-fit"
+              >
+                <span>Download CV</span>
+                <img
+                  src={DownloadCVIcon}
+                  alt="Icon Download CV"
+                  className="w-6 sm:w-14"
+                />
+              </a>
+            </div>
           </div>
         </div>
         <Quote
