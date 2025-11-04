d54f066013059dddd2278f091b6e4bed60883d89
BoviliusMeidi <boviliusbm@gmail.com>
2025-11-04 20:25:47 +0700

    fix(components) : fix the padding of main layout

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -3,9 +3,9 @@ import PropTypes from "prop-types"
 const MainLayout = ({ children, backgroundColor, center }) => {
   return (
     <div
-      className={`h-screen w-full relative ${backgroundColor} px-6 flex ${
+      className={`h-screen w-full relative ${backgroundColor} flex ${
         center ? "justify-center" : "justify-center lg:justify-normal"
-      } lg:px-48 md:px-12 overflow-hidden`}
+      } overflow-hidden`}
     >
       {children}
     </div>
