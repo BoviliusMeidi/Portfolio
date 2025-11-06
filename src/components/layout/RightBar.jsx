742eb171853610ae210979e539b0069e0efbf60d
BoviliusMeidi <boviliusbm@gmail.com>
2025-11-06 13:30:37 +0700

    style(layout) : change the style in rightbar of name of section

--- src/components/layout/RightBar.jsx
+++ src/components/layout/RightBar.jsx
@@ -4,26 +4,65 @@ import { HashLink } from "react-router-hash-link";
 const RightBar = ({ isOpen }) => {
   return (
     <div
-      className={`fixed top-0 left-0 w-full z-40 bg-rightBar rightBar p-12 flex flex-col justify-center items-center h-screen transform transition-transform duration-500 ${
+      className={`fixed top-0 left-0 w-full z-40 bg-rightBar rightBar p-12 gap-8 flex flex-col justify-center items-center h-screen transform transition-transform duration-500 ${
         isOpen ? "translate-y-0" : "-translate-y-full"
       }`}
     >
-      <a href="/">
-        HOME
+      <a href="/" className="group relative inline-block overflow-hidden">
+        <h1 className="text-5xl font-bold md:text-7xl lg:text-[7rem] transition-transform duration-500 ease-in-out group-hover:-translate-y-full">
+          HOME
+        </h1>
+        <h1
+          className="text-5xl font-bold md:text-7xl lg:text-[7rem] transition-transform duration-500 ease-in-out absolute top-full left-0 group-hover:-translate-y-full"
+          aria-hidden="true"
+        >
+          HOME
+        </h1>
       </a>
-      <a href="/projects">
-        PROJECTS
+      <a
+        href="/projects"
+        className="group relative inline-block overflow-hidden"
+      >
+        <h1 className="text-5xl font-bold md:text-7xl lg:text-[7rem] transition-transform duration-500 ease-in-out group-hover:-translate-y-full">
+          PROJECTS
+        </h1>
+        <h1
+          className="text-5xl font-bold md:text-7xl lg:text-[7rem] transition-transform duration-500 ease-in-out absolute top-full left-0 group-hover:-translate-y-full"
+          aria-hidden="true"
+        >
+          PROJECTS
+        </h1>
       </a>
-      <a href="/about">
-        ABOUT ME
+      <a href="/about" className="group relative inline-block overflow-hidden">
+        <h1 className="text-5xl font-bold md:text-7xl lg:text-[7rem] transition-transform duration-500 ease-in-out group-hover:-translate-y-full">
+          ABOUT ME
+        </h1>
+        <h1
+          className="text-5xl font-bold md:text-7xl lg:text-[7rem] transition-transform duration-500 ease-in-out absolute top-full left-0 group-hover:-translate-y-full"
+          aria-hidden="true"
+        >
+          ABOUT ME
+        </h1>
       </a>
-      <HashLink to="/#contact"> CONTACT </HashLink>
+      <HashLink to="/#contact">
+        <div className="group relative inline-block overflow-hidden">
+          <h1 className="text-5xl font-bold md:text-7xl lg:text-[7rem] transition-transform duration-500 ease-in-out group-hover:-translate-y-full">
+            CONTACT
+          </h1>
+          <h1
+            className="text-5xl font-bold md:text-7xl lg:text-[7rem] transition-transform duration-500 ease-in-out absolute top-full left-0 group-hover:-translate-y-full"
+            aria-hidden="true"
+          >
+            CONTACT
+          </h1>
+        </div>{" "}
+      </HashLink>
     </div>
   );
 };
 
 RightBar.propTypes = {
-  isOpen : PropTypes.bool,
-}
+  isOpen: PropTypes.bool,
+};
 
 export default RightBar;
